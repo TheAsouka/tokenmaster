eb128a7d295a24e15aed1766f69a3313b2e06392
MBDB <bapt857@gmail.com>
2023-06-06 16:09:30 +0200

    First Commit

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -5,9 +5,10 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TokenMaster is ERC721 {
     address public owner;
-    uint256 public totalOccasions;
-    uint256 public totalSupply;
+    uint256 public totalOccasions; //Event counter, start at 0 by default
 
+    //Occasion = Event => Struct define an Event, not create one
+    //Event is a reserved keywork in Sol, can't use it as variable name or else
     struct Occasion {
         uint256 id;
         string name;
@@ -19,20 +20,17 @@ contract TokenMaster is ERC721 {
         string location;
     }
 
+    //Key => value pair
+    // id => struct
     mapping(uint256 => Occasion) occasions;
-    mapping(uint256 => mapping(address => bool)) public hasBought;
-    mapping(uint256 => mapping(uint256 => address)) public seatTaken;
-    mapping(uint256 => uint256[]) seatsTaken;
 
-    modifier onlyOwner() {
+    modifier onlyOwner(){
         require(msg.sender == owner);
+        // _; -> Function body -> modifier is checked before function is executed
         _;
     }
 
-    constructor(
-        string memory _name,
-        string memory _symbol
-    ) ERC721(_name, _symbol) {
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol){
         owner = msg.sender;
     }
 
@@ -43,54 +41,23 @@ contract TokenMaster is ERC721 {
         string memory _date,
         string memory _time,
         string memory _location
-    ) public onlyOwner {
-        totalOccasions++;
-        occasions[totalOccasions] = Occasion(
-            totalOccasions,
-            _name,
-            _cost,
-            _maxTickets,
-            _maxTickets,
-            _date,
-            _time,
-            _location
-        );
-    }
+        ) public onlyOwner {
+            totalOccasions++;
 
-    function mint(uint256 _id, uint256 _seat) public payable {
-        // Require that _id is not 0 or less than total occasions...
-        require(_id != 0);
-        require(_id <= totalOccasions);
-
-        // Require that ETH sent is greater than cost...
-        require(msg.value >= occasions[_id].cost);
-
-        // Require that the seat is not taken, and the seat exists...
-        require(seatTaken[_id][_seat] == address(0));
-        require(_seat <= occasions[_id].maxTickets);
-
-        occasions[_id].tickets -= 1; // <-- Update ticket count
-
-        hasBought[_id][msg.sender] = true; // <-- Update buying status
-        seatTaken[_id][_seat] = msg.sender; // <-- Assign seat
-
-        seatsTaken[_id].push(_seat); // <-- Update seats currently taken
-
-        totalSupply++;
-
-        _safeMint(msg.sender, totalSupply);
+            //Use mapping -> To save data in the blockchain
+            occasions[totalOccasions] = Occasion(
+                totalOccasions,
+                _name,
+                _cost,
+                _maxTickets,
+                _maxTickets,
+                _date,
+                _time,
+                _location
+                );
     }
 
     function getOccasion(uint256 _id) public view returns (Occasion memory) {
         return occasions[_id];
-    }
-
-    function getSeatsTaken(uint256 _id) public view returns (uint256[] memory) {
-        return seatsTaken[_id];
-    }
-
-    function withdraw() public onlyOwner {
-        (bool success, ) = owner.call{value: address(this).balance}("");
-        require(success);
     }
 }
